024f5fcf019435b5f3d3564869c84a14d984267d
JFernando13 <juan_fer-13@hotmail.com>
2024-01-21 20:16:46 -0500

    add config path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
 import { defineConfig } from 'astro/config';
+import unocss from 'unocss/astro';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    unocss(
+      {
+        injectReset: true
+      }
+    ),
+    tailwind(
+      {
+        nesting: true,
+        applyBaseStyles: true
+      }
+    )
+  ]
+});
