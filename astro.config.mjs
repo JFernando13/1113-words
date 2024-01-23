215264dee6c2c1e0237dbc6893f78b26942bb14e
JFernando13 <juan_fer-13@hotmail.com>
2024-01-23 14:06:00 -0500

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro';
-
 import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    unocss(
-      {
-        injectReset: true
-      }
-    ),
-    tailwind(
-      {
-        nesting: true,
-        applyBaseStyles: true
-      }
-    )
-  ]
+  integrations: [unocss({
+    injectReset: true
+  }), tailwind({
+    nesting: true,
+    applyBaseStyles: true
+  }), svelte()],
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'es',
+    routing: {
+      prefixDefaultLocale: false
+    },
+  }
 });
