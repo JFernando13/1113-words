99edc43bba031e0468c967ceb8b1c90044186f3c
JFernando13 <juan_fer-13@hotmail.com>
2024-02-18 09:35:10 -0500

    fix structure of ui i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,9 +1,9 @@
 export const languages = {
   es: "es",
   en: "en",
-}
+};
 
-export const defaultLanguage = "es"
+export const defaultLanguage = "es";
 
 export const ui = {
   en: {
@@ -14,22 +14,26 @@ export const ui = {
       title: {
         1: "My Day",
         2: "in",
-        3: "words"
+        3: "words",
       },
-      subtitle: "The perfect summary of the day is the one that can be told in 1113 words",
-      btn: "show days"
+      subtitle:
+        "The perfect summary of the day is the one that can be told in 1113 words",
+      btn: "show days",
     },
     FOOTER: {
       links: {
         why: "why this project?",
-        who: "who I am?"
-      }
+        who: "who I am?",
+      },
     },
     BLOG: {
       title: "day",
-      btn: "show days",
-      btnFilter: "filter"
-    }
+      btn: "select day",
+      btnFilter: "filter",
+      CARD: {
+        day: "day",
+      },
+    },
   },
   es: {
     SEO: {
@@ -39,21 +43,24 @@ export const ui = {
       title: {
         1: "Mi Día",
         2: "en",
-        3: "palabras"
+        3: "palabras",
       },
-      subtitle: "El resumen perfecto del día es el que se puede contar en 1113 palabras",
-      btn: "mostrar dias"
+      subtitle:
+        "El resumen perfecto del día es el que se puede contar en 1113 palabras",
+      btn: "mostrar dias",
     },
     FOOTER: {
       links: {
         why: "por qué este proyecto?",
-        who: "quien soy?"
-      }
+        who: "quien soy?",
+      },
     },
     BLOG: {
-      title: "día",
-      btn: "mostrar dias",
-      btnFilter: "filtrar"
-    }
-  }
-}
+      btn: "seleccionar día",
+      btnFilter: "filtrar",
+      CARD: {
+        day: "día",
+      },
+    },
+  },
+};
