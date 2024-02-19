96ba1f42f61843eaa0f10a87ac0ceed239fa1c4c
JFernando13 <juan_fer-13@hotmail.com>
2024-02-19 07:51:09 -0500

    change from svelte to react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,26 @@
-import { defineConfig } from 'astro/config';
-import unocss from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
 import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [unocss({
-    injectReset: true
-  }), tailwind({
-    nesting: true,
-    applyBaseStyles: true
-  }), svelte()],
+  integrations: [
+    unocss({
+      injectReset: true,
+    }),
+    tailwind({
+      nesting: true,
+      applyBaseStyles: true,
+    }),
+    react(),
+  ],
   i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'es',
+    locales: ["en", "es"],
+    defaultLocale: "es",
     routing: {
-      prefixDefaultLocale: false
+      prefixDefaultLocale: false,
     },
-  }
+  },
 });
