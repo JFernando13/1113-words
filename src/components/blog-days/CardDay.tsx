0a09e2679d17d977426cae6e3ecb8214a45df8af
JFernando13 <juan_fer-13@hotmail.com>
2024-02-20 18:46:02 -0500

    fix navigate to days post

--- src/components/blog-days/CardDay.tsx
+++ src/components/blog-days/CardDay.tsx
@@ -26,20 +26,26 @@ export function CardDay({ data, currentLocale, id }: Props) {
   const favorites = useFavoriteStore((state) => state.favorites);
 
   const navigate = () => {
-    const url = `${currentLocale === "es" ? "" : "en"}/days/${id}`;
+    const currentPath = window.location.pathname;
+    // Construir la nueva URL con el idioma y el ID
+    const url = `${window.location.origin}${currentPath}/days/${id}`;
 
-    window.location.href = url;
+    // Redirigir a la nueva URL
+    window.location.replace(url);
   };
 
   return (
-    <article className={`${styles["day-card"]} group`} onClick={navigate}>
+    <article
+      className={`flex flex-col justify-between px-8 py-7 h-full max-w-[500px] rounded-lg bg-secondary-500 dark:bg-primary-600/30 gap-6 shadow-card-primary hover:scale-105 transition-transform duration-300group`}
+      onClick={navigate}
+    >
       <main className="flex flex-col gap-2">
         <header className="flex justify-between items-center">
           <p className={`${styles["day"]} flex items-center gap-2`}>
-            <span className="text-gray-300 text-sm">
+            <span className="text-secondary-300 dark:text-gray-300 text-sm">
               {t.BLOG.CARD.day} {data.day}
             </span>
-            <span className="text-xs font-thin text-gray-400">
+            <span className="text-xs font-thin text-secondary-300 dark:text-gray-400">
               {data.pubDate}
             </span>
           </p>
@@ -55,9 +61,9 @@ export function CardDay({ data, currentLocale, id }: Props) {
           >
             <SmileIcon
               className={clsx(
-                "w-6 h-6 text-gray-400 hover:text-gray-200 transition-colors",
+                "w-6 h-6 text-secondary-300 dark:text-gray-400 hover:text-gray-200 transition-colors",
                 favorites.some((f) => f.day === data.day) &&
-                  "text-yellow-500 hover:text-yellow-400",
+                  "dark:text-yellow-500 dark:hover:text-yellow-400 text-yellow-400 hover:text-yellow-100",
               )}
             />
           </button>
@@ -69,7 +75,12 @@ export function CardDay({ data, currentLocale, id }: Props) {
       >
         {data.tags.map((tag) => {
           return (
-            <span className={styles.tag} key={tag}>
+            <span
+              className={
+                "px-4 rounded-full py-1 bg-secondary-300 text-secondary-600 dark:bg-secondary-600 dark:text-secondary-300 text-xs capitalize transition-colors text-nowrap font-medium hover:bg-primary-400 dark:hover:bg-secondary-500"
+              }
+              key={tag}
+            >
               {tag}
             </span>
           );
