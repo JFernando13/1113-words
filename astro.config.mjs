19c13b87857f2f0db75944f0205af11e42918d6b
JFernando13 <juan_fer-13@hotmail.com>
2024-02-22 08:26:28 -0500

    update routing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import unocss from "unocss/astro";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -7,9 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    unocss({
-      injectReset: true,
-    }),
     tailwind({
       nesting: true,
       applyBaseStyles: true,
@@ -20,7 +16,9 @@ export default defineConfig({
     locales: ["en", "es"],
     defaultLocale: "es",
     routing: {
-      prefixDefaultLocale: false,
+      // prefixDefaultLocale: false,
+      strategy: "pathname",
     },
   },
+  output: "hybrid",
 });
