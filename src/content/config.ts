8a721116dd03e91dc95ea33fb723f23a6a5001f9
JFernando13 <juan_fer-13@hotmail.com>
2024-03-08 06:18:02 -0500

    fix config

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,8 +4,8 @@ const daysCollection = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
-    tags: z.array(z.string()),
+    pubDate: z.date().transform((date) => date.toISOString().split("T")[0]),
+    tags: z.optional(z.array(z.string())),
     day: z.number(),
     mood: z.string(),
   }),
